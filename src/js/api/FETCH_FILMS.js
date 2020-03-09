18bb21c62e1cf1ccd822a9b89aad219bc4a683bc
Roman Beniukh <ROMAN.BENIUKH@GMAIL.COM>
2020-03-09 20:46:08 +0200

    init commit

---
+++ src/js/api/FETCH_FILMS.js
@@ -0,0 +1,23 @@
+const baseUrl = `https://api.themoviedb.org/3/`;
+const apiKey = `4c70739ab1bc7f2c582885ab460406ce`;
+
+export default {
+  allFilms (page) {
+    const params = `movie/popular?language=ru-RU`;
+
+    return fetch(baseUrl + params + `&api_key=${apiKey}` + `&page=${page}`)
+      .then(response => response.json());
+  },
+  filmInfo (id) {
+    const params = `&language=ru-RU`;
+
+    return fetch(baseUrl + `movie/${id}` + `?api_key=${apiKey}` + params)
+      .then(response => response.json());
+  },
+  searchFilms (query, page) {
+    const params = `search/movie?language=ru-RU`;
+
+    return fetch(baseUrl + params + `&api_key=${apiKey}` + `&query=${query}` + `&page=${page}`)
+      .then(response => response.json());
+  },
+};
---
+++ src/js/main.js
@@ -0,0 +1,5 @@
+import FETCH_FILMS from "./api/FETCH_FILMS";
+
+FETCH_FILMS.allFilms(1).then(data => console.log(data));
+FETCH_FILMS.filmInfo(475303).then(data => console.log(data));
+FETCH_FILMS.searchFilms('game', 1).then(data => console.log(data));
